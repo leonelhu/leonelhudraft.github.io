151155c2aec49f46eabc677c81cb85d720ce809c
leonelhu <karlo42307@gmail.com>
2025-11-21 09:19:46 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,67 @@
-var modal = document.getElementById("modal");
-var btn = document.getElementById("關於古卡羅");
-var span = modal.querySelector('.close');
+var btn = document.querySelectorAll("button.modal-button");
 
-btn.addEventListener('click', function() {
+var modals = document.querySelectorAll('.modal');
+
+var spans = document.querySelectorAll(".close, .close2");
+
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
     modal.style.display = "block";
-  });
+ }
+}
 
-span.addEventListener('click', function() {
-  modal.style.display = 'none';
-});
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+}
+
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
+
+
+dragElement(document.getElementById("modal1"));
+dragElement(document.getElementById("modal2"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
